43676520fa09cd843e33439563d48e0717c7ee8c
hsisco <hysensisco@gmail.com>
2019-10-17 12:42:23 -0700

    Build out Form component structure

--- onboarding-process/src/components/Form.js
+++ onboarding-process/src/components/Form.js
@@ -4,10 +4,40 @@ import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-function OnboardForm ({}) {
+function Form ({}) {
   return (
     <Form>
-      
+      <label>
+        <Field
+        type="text"
+        name="name"
+        />
+        Name
+      </label>
+      <label>
+        <Field
+        type="email"
+        name="email"
+        />
+        Email
+      </label>
+      <label>
+        <Field
+        type="password"
+        name="password"
+        />
+        Password
+      </label>
+      <label>
+        <Field
+        type="checkbox"
+        name="accept"
+
+        />
+        Accept Terms of Service
+      </label>
+      <button>Submit</button>
     </Form>
   );
 }
+export default Form;
